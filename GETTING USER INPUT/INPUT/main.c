3e54a9948ed0b77097b0d0c8ba87eff594ec5cd1
hp <sahalnurdin@gmail.com>
2022-12-05 10:05:02 +0700

    add changes

--- GETTING USER INPUT/INPUT/main.c
+++ GETTING USER INPUT/INPUT/main.c
@@ -5,7 +5,8 @@ int main()
 {
     // CARA MEMINTA INPUT STRING DARI USER
     // CARA 1: NAMUN INI HANYA BISA DIPAKAI UNTUK KATA PERTAMA SEBELUM SPASI PERTAMA
-    char name[20];
+    char nameDepan[20];
+    char nameBelakang[20];
     printf("Masukkan nama kamu: ");
     // scanf("%s", name);
     // printf("Nama kamu adalah %s", name); // TIDAK MEMERLUKAN &
@@ -15,8 +16,12 @@ int main()
     // printf("Nama kamu adalah %s djnak", name); // djnak LANGSUNG KE BAWAH
 
     // CARA 3: PALING TEPAT
-    scanf("%[^\n]*c", name);
-    printf("Nama kamu adalah %s dajdajdajk", name);
+        printf("Masukkan nama kamu: ");
+    scanf("%[^\n]*c", nameDepan); // error
+    printf("Masukkan nama kamu: ");
+    scanf("%[^\n]*c", nameBelakang);
+    printf("Nama kamu adalah %s dajdajdajk", nameDepan);
+    printf("Nama kamu adalah %s dajdajdajk", nameBelakang);
 
     // UNTUK DOUBLE MAKA MENGGUNAKAN LF (LONG FLOAT)
     // double angka;
