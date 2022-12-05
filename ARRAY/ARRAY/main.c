ebc97b2dcce44af2cfc982820dd9cc6058a14ea3
hp <sahalnurdin@gmail.com>
2022-12-05 10:35:17 +0700

    add changes

--- ARRAY/ARRAY/main.c
+++ ARRAY/ARRAY/main.c
@@ -6,7 +6,7 @@ int main()
     int luckyNumber[] = {1,7,18, 9, 100};
     luckyNumber[0] = 1000;
     printf("%d\n",luckyNumber[0]);
-    char karakter[10];
+    char karakter[10]; // HARUS DISPESIFIKASIKAN
     karakter[0] = 'a';
     printf("%c",karakter[0]);
     return 0;
