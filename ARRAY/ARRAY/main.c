3abb81b1aaaa66efa31519f36aecfa17ab0a311a
hp <sahalnurdin@gmail.com>
2022-12-05 10:28:00 +0700

    add changes

---
+++ ARRAY/ARRAY/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luckyNumber[] = {1,7,18, 9, 100};
+    luckyNumber[0] = 1000;
+    printf("%d\n",luckyNumber[0]);
+    char karakter[10];
+    karakter[0] = 'a';
+    printf("%c",karakter[0]);
+    return 0;
+}
