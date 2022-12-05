5f69e62455a9091ca437cb6414fdffa6378c035c
hp <sahalnurdin@gmail.com>
2022-12-05 12:33:27 +0700

    add changes

---
+++ BUILDING A BETTER CALCULATOR/BUILDING A BETTER CALCULATOR/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // BUILDING A BETTER CALCULATOR
+    double num1;
+    double num2;
+    char op;
+    // INPUT
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+     printf("Enter an operator: ");
+    scanf("%c", &op);
+
+    // LOGIC
+    if (op == '+') {
+        printf("Answer: %f", num1 + num2);
+    } else if (op == '-'){
+         printf("Answer: %f", num1 - num2);
+    } else if (op == '*'){
+         printf("Answer: %f", num1 * num2);
+    } else if (op == '/'){
+         printf("Answer: %f", num1 / num2);
+    } else {
+        printf("Wrong operatior!!!");
+    }
+    return 0;
+}
