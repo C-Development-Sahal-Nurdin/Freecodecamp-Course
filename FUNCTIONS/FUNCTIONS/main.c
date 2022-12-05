0571ae0f6b8771d37ed3c283f5259f75dc9043e3
hp <sahalnurdin@gmail.com>
2022-12-05 10:41:32 +0700

    add changes

--- FUNCTIONS/FUNCTIONS/main.c
+++ FUNCTIONS/FUNCTIONS/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+// FUNGSI BARU MENGGUNAKAN VOID DI MANA TIDAK ADA RETURN STATEMENT
+void sayHi()
+{
+    printf("Hello World!!!\n");
+}
+
+// MENGGUNAKAN PARAMETER
+
+void sayHi1(char name[], int age)
+{
+    printf("Hello World!!! %s %d", name, age);
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    sayHi();
+    sayHi1("SAHAL",18);
     return 0;
 }
+
+
