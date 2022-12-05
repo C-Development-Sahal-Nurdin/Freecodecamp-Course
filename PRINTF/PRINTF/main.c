953acd1c8243ce2d4c8a203590133dab0b54f96a
hp <sahalnurdin@gmail.com>
2022-12-05 08:02:58 +0700

    add changes

---
+++ PRINTF/PRINTF/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // KITA MEMBUTUHKAN SEBUAH FORMAT SPECIFIER
+    int favNum = 100;
+    char myChar = 'i';
+    printf("My favourite %c is %d", myChar, favNum );
+    return 0;
+}
---
+++ WORKING WITH NUMBERS/Numbers/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%f",ceil(36.356));      // AKAN MEMBULATKAN BILANGAN KE ATAS
+    printf("%f", floor(36.356));    // AKAN MEMBULATKAN BILANGAN KE BAWAH
+    printf("%f", pow(4, 3));        // PANGKAT
+    printf("%f", sqrt(36));         // AKAR
+    return 0;
+}
