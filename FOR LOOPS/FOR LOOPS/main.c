370e2245b889a0915c6f628d1872e07a468b1d6e
hp <sahalnurdin@gmail.com>
2022-12-05 13:55:18 +0700

    add changes

---
+++ FOR LOOPS/FOR LOOPS/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int luckyNumber[] = {1, 4, 8, 7, 8, 18};
+
+    int i;
+    for (i = 0; i < 5; i++){
+        printf("%d\n", luckyNumber[i]); // DIMULAI DARI INDEKS 0 yaitu 1
+    }
+    return 0;
+}
