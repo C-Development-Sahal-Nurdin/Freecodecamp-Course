eabe28286263c4611d7c46ea8d50bceb0b57c500
hp <sahalnurdin@gmail.com>
2022-12-05 09:41:22 +0700

    add changes

---
+++ BUILDING A BASIC CALCULATOR/CALCULATOR/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double num1, num2;
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter second number: ");
+    scanf("%lf", &num1);
+
+    printf("Amswer: %f", num1 + num2);
+    return 0;
+}
--- GETTING USER INPUT/INPUT/main.c
+++ GETTING USER INPUT/INPUT/main.c
@@ -6,7 +6,7 @@ int main()
     // CARA MEMINTA INPUT STRING DARI USER
     // CARA 1: NAMUN INI HANYA BISA DIPAKAI UNTUK KATA PERTAMA SEBELUM SPASI PERTAMA
     char name[20];
-    printf("Masukkan nama kamu: ");
+    // printf("Masukkan nama kamu: ");
     // scanf("%s", name);
     // printf("Nama kamu adalah %s", name); // TIDAK MEMERLUKAN &
 
@@ -15,8 +15,14 @@ int main()
     // printf("Nama kamu adalah %s djnak", name); // djnak LANGSUNG KE BAWAH
 
     // CARA 3: PALING TEPAT
-    scanf("%[^\n]*c", name);
-    printf("Nama kamu adalah %s dajdajdajk", name);
+    // scanf("%[^\n]*c", name);
+    // printf("Nama kamu adalah %s dajdajdajk", name);
+
+    // UNTUK DOUBLE MAKA MENGGUNAKAN LF (LONG FLOAT)
+    double angka;
+    printf("Masukkan angka kamu: ");
+    scanf("%lf", &angka);
+    printf("Angka kamu adalah %lf", angka);
 
     return 0;
 }
