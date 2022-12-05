326b055c2fbe10f360b94f627d29a454830c1027
hp <sahalnurdin@gmail.com>
2022-12-05 08:12:53 +0700

    add changes

---
+++ CONSTANTS/CONSTANTS/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int FAV_NUM = 100;
+    printf("%d\n", num);
+    num = 8; // INI AKAN ERROR
+    printf("%d\n", num);
+    printf("");
+
+    //INI JUGA TERMASUK KE DALAM CONSTANT ATAU KONSTANTA
+    printf("Hallo");
+    printf("%d", 90);
+
+    return 0;
+}
