714a0847cc2cb17597df5edd439a339e97aabab8
hp <sahalnurdin@gmail.com>
2022-12-05 07:47:17 +0700

    add changes

---
+++ DATA TYPES/Data Types/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 19;
+    float gpa = 4.00;
+    char grade = 'A';
+    char phrase[] = "Kamu menadapat nilai sempurna."
+    return 0;
+}
