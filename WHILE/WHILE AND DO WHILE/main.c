9fcf2e81f5340e42b560eb3227e0740890ef0023
hp <sahalnurdin@gmail.com>
2022-12-05 13:32:39 +0700

    add changes

---
+++ WHILE/WHILE AND DO WHILE/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // WHILE
+    int x = 1;
+    while(x <=5) {
+        printf("%d\n", x);
+        x++;
+    }
+    //DO WHILE
+    int INDEKS = 6;
+    do{
+        printf("%d\n", INDEKS);
+        x++;
+    }while(INDEKS <=5);
+
+    return 0;
+}
