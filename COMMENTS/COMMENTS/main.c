49e3eddc235b85a043b1dd88dc12a67b4ce03dc6
hp <sahalnurdin@gmail.com>
2022-12-05 08:07:11 +0700

    add changes

---
+++ COMMENTS/COMMENTS/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n"); // SINGLE LINE
+    // MULTI LINE
+    /*
+     printf("Hello World!");
+    */
+
+    return 0;
+}
